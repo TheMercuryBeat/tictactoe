25806d1fb237885d60d69b6404bba4e07b1f6e5b
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-29 17:12:16 +0100

    Creado constructor en ResumeController y modificado el ResumeControllerTest al crear el objecto resumeController que tambien se le pasa el State

--- src/test/java/usantatecla/tictactoe/controllers/ResumeControllerTest.java
+++ src/test/java/usantatecla/tictactoe/controllers/ResumeControllerTest.java
@@ -1,6 +1,7 @@
 package usantatecla.tictactoe.controllers;
 
 import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.models.State;
 
 import static org.mockito.Mockito.verify;
 
@@ -8,7 +9,7 @@ public class ResumeControllerTest extends ControllerTest {
 
     @Override
     protected Controller createController() {
-        return new ResumeController(new Game());
+        return new ResumeController(new Game(), new State());
     }
 
     @Override
