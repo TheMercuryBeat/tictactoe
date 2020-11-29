3ba8b009105d3092cc467d93903f0b82d9cbfe6a
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-29 17:57:15 +0100

    Añadir a los controladores del Logic el State a traves del controlador

--- src/main/java/usantatecla/tictactoe/controllers/Logic.java
+++ src/main/java/usantatecla/tictactoe/controllers/Logic.java
@@ -18,9 +18,9 @@ public class Logic {
         this.game = new Game();
         this.state = new State();
         this.controllers = new HashMap<>();
-        this.controllers.put(StateValue.INITIAL, new StartController(this.game));
-        this.controllers.put(StateValue.IN_GAME, new PlayController(this.game));
-        this.controllers.put(StateValue.RESUME, new ResumeController(this.game));
+        this.controllers.put(StateValue.INITIAL, new StartController(this.game, this.state));
+        this.controllers.put(StateValue.IN_GAME, new PlayController(this.game, this.state));
+        this.controllers.put(StateValue.RESUME, new ResumeController(this.game, this.state));
         this.controllers.put(StateValue.EXIT, null);
     }
 
