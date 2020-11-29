b5002c30db5ffe2e9e6c139515e2fd1284521eb6
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-29 11:32:14 +0100

    Creado un enumerado con los estados del juego, una clase para resetear, avanzar y obtener el valor del estado actual del juego y una clase de test, la cual falla por no tener logica en el State

---
+++ src/main/java/usantatecla/tictactoe/models/State.java
@@ -0,0 +1,21 @@
+package usantatecla.tictactoe.models;
+
+
+public class State {
+
+    private StateValue stateValue;
+
+    public State() {
+    }
+
+    public void reset() {
+    }
+
+    public void next() {
+    }
+
+    public StateValue getValueState() {
+        return null;
+    }
+
+}
---
+++ src/test/java/usantatecla/tictactoe/models/StateTest.java
@@ -0,0 +1,23 @@
+package usantatecla.tictactoe.models;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StateTest {
+
+    private State state;
+
+    public StateTest() {
+        this.state = new State();
+    }
+
+    @Test
+    public void testAnyStateWhenGetStateValueResetShouldReturnTheStateValue() {
+        assertThat(this.state.getValueState(), is(equalTo(StateValue.INITIAL)));
+    }
+
+}
