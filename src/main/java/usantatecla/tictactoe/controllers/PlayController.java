7a6568cd8c780cef5a1d7df3e08187b3d3a205c5
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-29 17:10:55 +0100

    Creado constructor en PlayController y modificado el PlayControllerTest al crear el objecto playController que tambien se le pasa el State

--- src/main/java/usantatecla/tictactoe/controllers/PlayController.java
+++ src/main/java/usantatecla/tictactoe/controllers/PlayController.java
@@ -2,6 +2,7 @@ package usantatecla.tictactoe.controllers;
 
 import usantatecla.tictactoe.models.Coordinate;
 import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.models.State;
 import usantatecla.tictactoe.models.Token;
 import usantatecla.tictactoe.types.Error;
 
@@ -9,6 +10,10 @@ public class PlayController extends Controller {
 
 	public PlayController(Game game) {
 		super(game);
+	}
+
+	public PlayController(Game game, State state) {
+		super(game, state);
 	}
 
 	public boolean isBoardComplete() {
